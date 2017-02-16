76bf1b80595ab877539ca6971e019af473bae529
Yann BECRET <ybecret@e1r12p4.42.fr>
2017-02-16 19:26:12 +0100

    nouvelle structure

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: ybecret <ybecret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 18:29:17 by ybecret           #+#    #+#             */
-/*   Updated: 2017/02/16 17:10:37 by ybecret          ###   ########.fr       */
+/*   Updated: 2017/02/16 18:31:49 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,39 @@ int     nb_tetriminos(char *buff)
         return ((i - cnt) / 16);
 }
 
-void	fill_tetris(s_tetris tetris, char *buff, int nb_piece)
+void	fill_tetris(s_tetris *tetris, char *buff, int nb_piece)
 {
-	int	i;
-	int j;
+	int		i;
+	int		j;
+	int		k;
+	char	tmp[16];
 
-	i = 0;
-	
+	j = 0;
+	while (j < nb_piece && *buff)
+	{
+		k = 0;
+		i = 0;
+		while (*buff == '.' || *buff == '\n')
+			i++;
+		while (i < 21)
+		{
+			if (*buff == '.' || *buff == '#')
+			{
+				tmp[k] = (*buff == '.' ? '0' : '1');
+				k++;
+			} 
+			buff++;
+			i++;
+		}
+		if (k < 16 && i == 20)
+		{
+			while (k++ < 16)
+				tmp[k] = '0';
+		}
+		tetris[j].piece = ft_strcpy(tetris[j].piece, tmp);
+		tetris[j]->index = j;
+		j++;
+	}
 }
 
 int		fillit(int fd)
